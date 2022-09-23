8ec3eeadda4144194560168e1d99fc025cee3246
Lupita Guerrero <guerreroguadalupe89@gmail.com>
2022-09-23 01:29:40 -0700

    last commit- Authenticator complete and working as expected

--- playground.rb
+++ playground.rb
@@ -6,12 +6,23 @@ users = [
 	{ username: "heisenberg", password: "password5" },
 ]
 
+def auth_user(usernameEntry, passwordEntry, list_of_users)
+	#authenticating
+	list_of_users.each do |user_record|
+		if user_record[:username] == usernameEntry && user_record[:password] == passwordEntry
+			return user_record
+		end
+	end
+	"Credentials were not correct"
+end
+
+
 puts "Welcome to the authenticator"
 25.times { print "-"}
 puts
 puts "This program will take input from the user and compare password"
 puts "If password is correct, you will get back the user object"
-# puts
+
 
 
 attempts = 1
@@ -20,33 +31,12 @@ while attempts < 4
 	usernameEntry = gets.chomp
 	print "Password: "
 	passwordEntry = gets.chomp
-
-	#authenticate
-	users.each do |user|
-		if user[:username] == usernameEntry && user[:password] == passwordEntry
-			puts user
-		else
-			puts "Credentials were not correct"
-		end
-	end
-
-
-
+	authentication = auth_user(usernameEntry, passwordEntry, users)
+	puts authentication
 	puts "Press n to quit or any other key to continue: "
 	input = gets.chomp.downcase
 	break if input == "n"
 	attempts += 1
 end
+puts "You have exceeded the number of attempts" if attempts == 4
 
-
-# def wrongCredentials
-# 	puts "Credentials were not correct"
-# 	puts "Press n to quit or any other key to continue: "	
-# 	#goes back to take username and password
-# end
-
-# def correctCredentials
-# 	#prints 	
-# end
-
-# "You have exceeded the number of attempts"
